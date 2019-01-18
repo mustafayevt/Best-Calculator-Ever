771aef636153cb5f3aeefbab707add0b7e4271fc
Tural Mustafayev <mustafayevt@users.noreply.github.com>
2019-01-19 01:55:41 +0400

    Add project files.

---
+++ CalculatorWinForms/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace CalculatorWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MainDisplayLbl = new System.Windows.Forms.Label();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.cBtn = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.delBtn = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.pointBtn = new System.Windows.Forms.Button();
+            this.divBtn = new System.Windows.Forms.Button();
+            this.multBtn = new System.Windows.Forms.Button();
+            this.minusBtn = new System.Windows.Forms.Button();
+            this.plusBtn = new System.Windows.Forms.Button();
+            this.equalBtn = new System.Windows.Forms.Button();
+            this.MainGrp = new System.Windows.Forms.GroupBox();
+            this.allOpsLbl = new System.Windows.Forms.Label();
+            this.negativeBtn = new System.Windows.Forms.Button();
+            this.MainGrp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainDisplayLbl
+            // 
+            resources.ApplyResources(this.MainDisplayLbl, "MainDisplayLbl");
+            this.MainDisplayLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MainDisplayLbl.Name = "MainDisplayLbl";
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.Black;
+            this.btn7.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.btn7, "btn7");
+            this.btn7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn7.Name = "btn7";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Black;
+            this.btn4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.btn4, "btn4");
+            this.btn4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn4.Name = "btn4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Black;
+            this.btn1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.btn1, "btn1");
+            this.btn1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn1.Name = "btn1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // cBtn
+            // 
+            this.cBtn.BackColor = System.Drawing.Color.Black;
+            this.cBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.cBtn, "cBtn");
+            this.cBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cBtn.Name = "cBtn";
+            this.cBtn.UseVisualStyleBackColor = false;
+            this.cBtn.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.Black;
+            this.btn8.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.btn8, "btn8");
+            this.btn8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn8.Name = "btn8";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.Black;
+            this.btn5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.btn5, "btn5");
+            this.btn5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn5.Name = "btn5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Black;
+            this.btn2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.btn2, "btn2");
+            this.btn2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn2.Name = "btn2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.Black;
+            this.btn0.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.btn0, "btn0");
+            this.btn0.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn0.Name = "btn0";
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // delBtn
+            // 
+            this.delBtn.BackColor = System.Drawing.Color.Black;
+            this.delBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.delBtn, "delBtn");
+            this.delBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.delBtn.Name = "delBtn";
+            this.delBtn.UseVisualStyleBackColor = false;
+            this.delBtn.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.Black;
+            this.btn9.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.btn9, "btn9");
+            this.btn9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn9.Name = "btn9";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.Black;
+            this.btn6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.btn6, "btn6");
+            this.btn6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn6.Name = "btn6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Black;
+            this.btn3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.btn3, "btn3");
+            this.btn3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn3.Name = "btn3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // pointBtn
+            // 
+            this.pointBtn.BackColor = System.Drawing.Color.Black;
+            this.pointBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.pointBtn, "pointBtn");
+            this.pointBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pointBtn.Name = "pointBtn";
+            this.pointBtn.UseVisualStyleBackColor = false;
+            this.pointBtn.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // divBtn
+            // 
+            this.divBtn.BackColor = System.Drawing.Color.Black;
+            this.divBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.divBtn, "divBtn");
+            this.divBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.divBtn.Name = "divBtn";
+            this.divBtn.UseVisualStyleBackColor = false;
+            this.divBtn.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // multBtn
+            // 
+            this.multBtn.BackColor = System.Drawing.Color.Black;
+            this.multBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.multBtn, "multBtn");
+            this.multBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.multBtn.Name = "multBtn";
+            this.multBtn.UseVisualStyleBackColor = false;
+            this.multBtn.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // minusBtn
+            // 
+            this.minusBtn.BackColor = System.Drawing.Color.Black;
+            this.minusBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.minusBtn, "minusBtn");
+            this.minusBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.minusBtn.Name = "minusBtn";
+            this.minusBtn.UseVisualStyleBackColor = false;
+            this.minusBtn.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // plusBtn
+            // 
+            this.plusBtn.BackColor = System.Drawing.Color.Black;
+            this.plusBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.plusBtn, "plusBtn");
+            this.plusBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.plusBtn.Name = "plusBtn";
+            this.plusBtn.UseVisualStyleBackColor = false;
+            this.plusBtn.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // equalBtn
+            // 
+            this.equalBtn.BackColor = System.Drawing.Color.Black;
+            this.equalBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.equalBtn, "equalBtn");
+            this.equalBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.equalBtn.Name = "equalBtn";
+            this.equalBtn.UseVisualStyleBackColor = false;
+            this.equalBtn.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // MainGrp
+            // 
+            this.MainGrp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            resources.ApplyResources(this.MainGrp, "MainGrp");
+            this.MainGrp.Controls.Add(this.equalBtn);
+            this.MainGrp.Controls.Add(this.plusBtn);
+            this.MainGrp.Controls.Add(this.minusBtn);
+            this.MainGrp.Controls.Add(this.negativeBtn);
+            this.MainGrp.Controls.Add(this.multBtn);
+            this.MainGrp.Controls.Add(this.divBtn);
+            this.MainGrp.Controls.Add(this.pointBtn);
+            this.MainGrp.Controls.Add(this.btn3);
+            this.MainGrp.Controls.Add(this.btn6);
+            this.MainGrp.Controls.Add(this.btn9);
+            this.MainGrp.Controls.Add(this.delBtn);
+            this.MainGrp.Controls.Add(this.btn0);
+            this.MainGrp.Controls.Add(this.btn2);
+            this.MainGrp.Controls.Add(this.btn5);
+            this.MainGrp.Controls.Add(this.btn8);
+            this.MainGrp.Controls.Add(this.cBtn);
+            this.MainGrp.Controls.Add(this.btn1);
+            this.MainGrp.Controls.Add(this.btn4);
+            this.MainGrp.Controls.Add(this.btn7);
+            this.MainGrp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MainGrp.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MainGrp.Name = "MainGrp";
+            this.MainGrp.TabStop = false;
+            // 
+            // allOpsLbl
+            // 
+            resources.ApplyResources(this.allOpsLbl, "allOpsLbl");
+            this.allOpsLbl.ForeColor = System.Drawing.SystemColors.Window;
+            this.allOpsLbl.Name = "allOpsLbl";
+            // 
+            // negativeBtn
+            // 
+            this.negativeBtn.BackColor = System.Drawing.Color.Black;
+            this.negativeBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.negativeBtn, "negativeBtn");
+            this.negativeBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.negativeBtn.Name = "negativeBtn";
+            this.negativeBtn.UseVisualStyleBackColor = false;
+            this.negativeBtn.Click += new System.EventHandler(this.equalBtn_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.Controls.Add(this.allOpsLbl);
+            this.Controls.Add(this.MainGrp);
+            this.Controls.Add(this.MainDisplayLbl);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MainGrp.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label MainDisplayLbl;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button cBtn;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button delBtn;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button pointBtn;
+        private System.Windows.Forms.Button divBtn;
+        private System.Windows.Forms.Button multBtn;
+        private System.Windows.Forms.Button minusBtn;
+        private System.Windows.Forms.Button plusBtn;
+        private System.Windows.Forms.Button equalBtn;
+        private System.Windows.Forms.GroupBox MainGrp;
+        private System.Windows.Forms.Label allOpsLbl;
+        private System.Windows.Forms.Button negativeBtn;
+    }
+}
+
---
+++ CalculatorWinForms/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.XPath;
+
+namespace CalculatorWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string equalMultiPress = string.Empty;
+        bool isOperand, equalPressed = false;
+        string lastOp = string.Empty;
+        char[] ops = { '+', '-', '*', '/' };
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if((e.KeyCode>=Keys.NumPad0 && e.KeyCode <= Keys.NumPad9) || (e.KeyCode>=Keys.D0&&e.KeyCode<=Keys.D9))
+            {
+                string tmp = "btn";
+                tmp += e.KeyCode.ToString().Substring(e.KeyCode.ToString().Length - 1);
+                (MainGrp.Controls[tmp] as Button).PerformClick();
+            }
+            else
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Multiply: (MainGrp.Controls["multBtn"] as Button).PerformClick();break;
+                    case Keys.Divide: (MainGrp.Controls["divBtn"] as Button).PerformClick(); break;
+                    case Keys.Subtract: (MainGrp.Controls["minusBtn"] as Button).PerformClick(); break;
+                    case Keys.Add: (MainGrp.Controls["plusBtn"] as Button).PerformClick(); break;
+                    case Keys.Oemplus: (MainGrp.Controls["equalBtn"] as Button).PerformClick(); break;
+                    case Keys.Back: (MainGrp.Controls["delBtn"] as Button).PerformClick(); break;
+                    case Keys.C: (MainGrp.Controls["cBtn"] as Button).PerformClick(); break;
+                    case Keys.OemPeriod:
+                    case Keys.N: (MainGrp.Controls["negativeBtn"] as Button).PerformClick(); break;
+                    case Keys.Decimal: (MainGrp.Controls["pointBtn"] as Button).PerformClick(); break;
+                }
+            }
+        }
+        private void equalBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var btn = (sender as Button).Text.ToLower();
+                switch (btn)
+                {
+                    case "c":
+                        MainDisplayLbl.Text = string.Empty;
+                        allOpsLbl.Text = string.Empty;
+                        break;
+                    case "<<":
+                        if (MainDisplayLbl.Text != string.Empty)
+                            MainDisplayLbl.Text = MainDisplayLbl.Text.Remove(MainDisplayLbl.Text.Length - 1);
+                        break;
+                    case ".":
+                        if (!MainDisplayLbl.Text.Contains('.') && MainDisplayLbl.Text != string.Empty)
+                            MainDisplayLbl.Text += btn;
+                        break;
+                    case "±":
+                        if (!MainDisplayLbl.Text.Contains('-')) MainDisplayLbl.Text = MainDisplayLbl.Text.Insert(0, "-");
+                        else MainDisplayLbl.Text = MainDisplayLbl.Text.Remove(0, 1);
+                        break;
+                    case "-":
+                    case "+":
+                    case "*":
+                    case "/":
+                        if (!isOperand && MainDisplayLbl.Text != string.Empty)
+                        {
+                            if (MainDisplayLbl.Text != string.Empty)
+                                if (MainDisplayLbl.Text.Last() == '.') MainDisplayLbl.Text = MainDisplayLbl.Text.Remove(MainDisplayLbl.Text.Length - 1);
+                            if (equalPressed) allOpsLbl.Text = MainDisplayLbl.Text;
+                            else allOpsLbl.Text += MainDisplayLbl.Text;
+                            allOpsLbl.Text += btn;
+                            lastOp = btn;
+                            MainDisplayLbl.Text = string.Empty;
+                            isOperand = true;
+                        }
+                        break;
+                    case "=":
+                        if (equalPressed && double.TryParse(allOpsLbl.Text.Last().ToString(), out double result))
+                        {
+                            try
+                            {
+                                allOpsLbl.Text += allOpsLbl.Text.Substring(allOpsLbl.Text.LastIndexOfAny(ops));
+                                MainDisplayLbl.Text = new DataTable().Compute(allOpsLbl.Text, null).ToString();
+                            }
+                            catch (Exception) { }
+                            return;
+                        }
+
+                        if (allOpsLbl.Text != string.Empty)
+                        {
+                            equalPressed = true;
+                            allOpsLbl.Text += MainDisplayLbl.Text;
+                            if (MainDisplayLbl.Text == string.Empty) allOpsLbl.Text = allOpsLbl.Text.Remove(allOpsLbl.Text.Length - 1);
+                            MainDisplayLbl.Text = new DataTable().Compute(allOpsLbl.Text, null).ToString();
+                            equalMultiPress = allOpsLbl.Text;
+                        }
+                        break;
+                    default:
+                        isOperand = false;
+                        MainDisplayLbl.Text += btn;
+                        break;
+                }
+            }
+            catch (Exception) { }
+            equalBtn.Focus();
+        }
+    }
+}
